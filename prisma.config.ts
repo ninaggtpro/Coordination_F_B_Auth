21f30a711e7ceeff8279f9e9ea3d1d83d701b978
ludovic <ludovic.andreotti@ynov.com>
2026-02-06 16:30:28 +0100

    FEAT :change prisma destination migration

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,8 +4,8 @@ import { defineConfig, env } from 'prisma/config';
 export default defineConfig({
   schema: 'src/infrastructure/database/prisma/schema.prisma',
   migrations: {
-    path: 'prisma/migrations',
-    seed: "ts-node prisma/seed.ts",
+    path: 'src/infrastructure/database/prisma/migrations',
+    seed: "ts-node src/infrastructure/database/prisma/seed.ts",
   },
   datasource: {
     url: env('DATABASE_URL'),
