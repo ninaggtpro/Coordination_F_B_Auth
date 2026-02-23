1ae1b9063cd73059f3acc2c590d603e3376d7f42
ninaggtpro <ninaguiguetpro@gmail.com>
2026-02-23 11:56:17 +0100

    Fix: configure JwtModule with secret from environment

--- src/presentation/controllers/auth/auth.module.ts
+++ src/presentation/controllers/auth/auth.module.ts
@@ -6,26 +6,32 @@ import { AuthController } from './auth.controller';
 import { LoginAccountUseCase } from 'src/application/use-cases/LoginAccount/LoginAccountUseCase';
 import { TokenService } from 'src/domain/services/token.service';
 import { LoginAccountValidator } from 'src/application/use-cases/LoginAccount/LoginAccountValidator';
-import { JwtService } from '@nestjs/jwt';
-import { CreateAccountValidator } from 'src/application/use-cases/CreateAccount/CreateAccountValidator';
+import { JwtModule } from '@nestjs/jwt';
 import { RefreshTokenController } from '../refreshToken/RefreshToken.controller';
 import { ValidTokenController } from '../validToken/ValidToken.controller';
 import { RefreshAccountUseCase } from 'src/application/use-cases/RefreshAccount/RefreshAccountUseCase';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule, RepositoriesModule],
+  imports: [
+    PrismaModule, 
+    RepositoriesModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'), // Il va chercher ta clé dans le .env
+        signOptions: { expiresIn: '60m' },
+      }),
+    }),
+  ],
   controllers: [AuthController, RefreshTokenController, ValidTokenController],
   providers: [
     LoginAccountUseCase,
     LoginAccountValidator,
     TokenService,
     AuthService,
-    JwtService,
     RefreshAccountUseCase,
-    // {
-    //   provide: IAccountRepository,
-    //   useClass: PrismaAccountRepository, 
-    // },
   ],
 })
 export class AuthModule {}
