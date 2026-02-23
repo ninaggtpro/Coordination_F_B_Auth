43037898dd72f9f18256a1e4f6cd8103d323dfa7
ludovic <ludovic.andreotti@ynov.com>
2026-02-23 09:54:25 +0100

    feat : mise en ligne

--- prisma.config.ts
+++ prisma.config.ts
@@ -5,9 +5,9 @@ export default defineConfig({
   schema: 'src/infrastructure/database/prisma/schema.prisma',
   migrations: {
     path: 'src/infrastructure/database/prisma/migrations',
-    seed: "ts-node src/infrastructure/database/prisma/seed.ts",
+    seed: 'ts-node src/infrastructure/database/prisma/seed.ts',
   },
   datasource: {
-    url: env('DATABASE_URL'),
+    url: process.env.DATABASE_URL,
   },
 });
